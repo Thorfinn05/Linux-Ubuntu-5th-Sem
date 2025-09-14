672cc7cb490e11db1300464b390c137339e42f4f
Thorfinn05 <rudranild04@gmail.com>
2025-09-14 09:18:06 +0000

    a program where the child prints numbers from 1 to 5, and the parent waits for the child before printing its own message

---
+++ as4_4.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<stdlib.h>
+#include<sys/wait.h>
+int main() {
+	pid_t pid = fork();
+	if(pid == 0){
+		printf("Child running\n");
+		int i;
+		for (i=1; i<=5; i++){
+			printf("%d ", i);
+		}
+	}
+	else {
+		wait(NULL);
+		printf("\nParent running\n");
+	}
+	return 0;
+}
+
